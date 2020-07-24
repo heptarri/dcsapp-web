aa4aacfef30c3200934279b79caada168331bbfc
QINGDKJ <qingdkj@outlook.com>
2020-07-24 13:16:00 +0800

    删除无用注释，添加dist目录

--- src/App.vue
+++ src/App.vue
@@ -6,72 +6,11 @@
 
 <template>
   <div id="app">
-    <!--    <img src="./assets/logo.png">-->
-    <!--    <router-view/>-->
 
-    <!--
-    <div class="left">
-
-        <br>
-        <h2 class="title" @click="clickTitle()">DCSAPP</h2>
-
-        <div class="biaoqian">
-          <router-link to="/" class="biaoqian">主页</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/programming" class="biaoqian">编程开发</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/themes" class="biaoqian">主题美化</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/musicandsound" class="biaoqian">音乐欣赏</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/games" class="biaoqian">游戏娱乐</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/photos" class="biaoqian">作图与看图</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/relations" class="biaoqian">社交软件</router-link>
-        </div>
-
-        <div class="biaoqian">
-          <router-link to="/tools" class="biaoqian">实用工具</router-link>
-        </div>
-
-         <div class="biaoqian">
-          <router-link to="/network" class="biaoqian">网络应用</router-link>
-        </div>
-
-         <div class="biaoqian">
-          <router-link to="/office" class="biaoqian">办公学习</router-link>
-        </div>
-
-         <div class="biaoqian">
-          <router-link to="/reading" class="biaoqian">阅读写作</router-link>
-        </div>
-
-         <div class="biaoqian">
-          <router-link to="/videos" class="biaoqian">视频观看</router-link>
-        </div>
-
-         <div class="biaoqian">
-          <router-link to="/others" class="biaoqian">其他应用</router-link>
-        </div>
-
-    </div>
-    -->
 
     <div class="right">
       <center>
+<!--        展示应用列表部分 (router)-->
         <router-view class="showRouterPage"></router-view>
       </center>
     </div>
