38e29aeaa97a268f26a3387749f32be2bcc01b4a
beyond-1234 <beyond-1234>
2020-08-21 21:20:06 +0800

    更改图片和applist.json的源为jerrywang的源

--- src/components/categoryComponent.vue
+++ src/components/categoryComponent.vue
@@ -1,80 +1,92 @@
 <template>
-  <div class="app">
-    <br/>
-    <br/>
-    <center>
-
-      <div class="list">
-
-        <!--        单个应用模块-->
-        <div class="item" v-for="item in list" :key="item.tip" @click="GotoJson(item.Pkgname)">
-
-          <span class="show">
-<!--            应用icon-->
-            <img :src="`http://img.shenmo.tech:38324/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
-                 class="icon-m" @click="GotoJson(item.Pkgname)"/>
-            <span class="app-title">
-<!--              应用名-->
-            <div>
-             <h3 class="app-name" :title="`${item.Name}`">{{item.Name}}</h3>
-             </div>
-            <div>
-             <h4 class="app-more" :title="`${item.More}`">{{item.More}}</h4>
-            </div>
-           </span>
-          </span>
-<!--
+    <div class="app">
+        <br />
+        <br />
+        <center>
+            <div class="list">
+                <!--        单个应用模块-->
+                <div
+                    class="item"
+                    v-for="item in list"
+                    :key="item.tip"
+                    @click="GotoJson(item.Pkgname)"
+                >
+                    <span class="show">
+                        <!--            应用icon-->
+                        <img
+                            :src="`${imgSource}/store/${category}/${item.Pkgname}/icon.png`"
+                            alt="icon"
+                            class="icon-m"
+                            @click="GotoJson(item.Pkgname)"
+                        />
+                        <span class="app-title">
+                            <!--              应用名-->
+                            <div>
+                                <h3 class="app-name" :title="`${item.Name}`">{{item.Name}}</h3>
+                            </div>
+                            <div>
+                                <h4 class="app-more" :title="`${item.More}`">{{item.More}}</h4>
+                            </div>
+                        </span>
+                    </span>
+                    <!--
           <img :src="`http://img.shenmo.tech:38324/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
                class="icon-bg" @click="GotoJson(item.Pkgname)"/>
--->
-        </div>
-      </div>
-    </center>
-  </div>
+                    -->
+                </div>
+            </div>
+        </center>
+    </div>
 </template>
 
 <script>
-  import axios from "axios";
+import axios from "axios";
 
-  export default {
+export default {
     name: "categoryComponent",
     //category用来在App.vue中区分类别
     //category名称就是分类名
-    props: ['category'],
+    props: ["category"],
     data() {
-      return {
-        list: [],
-        downloadContent: "DOWNLOAD"
-      };
+        return {
+            list: [],
+            downloadContent: "DOWNLOAD",
+            source: "https://store1.jerrywang.top",
+            imgSource: "http://cdn.jerrywang.top",
+        };
     },
     methods: {
-      getInfo() {
-        axios
-          //39.106.2.2:38324
-          .get(`http://img.shenmo.tech:38324/store/${this.category}/applist.json`)
-          //applist.json 软件列表
-          .then(res => {
-            this.list = res.data;
-          });
-      },
-      GotoJson(pkgn) {
-        console.log(
-          `http://img.shenmo.tech:38324/store/${this.category}/${pkgn}/app.json`
-          //app.json 软件的详细信息
-        );
-        window.open(
-          //pkgn 在仓库中的包名
-          `http://img.shenmo.tech:38324/store/${this.category}/${pkgn}/app.json`, '_self', ''
-        );
-      }
+        getInfo() {
+            axios
+                //39.106.2.2:38324
+                .get(
+                    `${this.source}/store/${this.category}/applist.json`
+                )
+                //applist.json 软件列表
+                .then((res) => {
+                    this.list = res.data;
+                });
+        },
+        GotoJson(pkgn) {
+            console.log(
+                `${this.source}/store/${this.category}/${pkgn}/app.json`
+                //app.json 软件的详细信息
+            );
+            window.open(
+                //pkgn 在仓库中的包名
+                `${this.source}/store/${this.category}/${pkgn}/app.json`,
+                "_self",
+                ""
+            );
+        },
     },
     mounted() {
-      this.getInfo();
-    }
-  };
+        this.getInfo();
+    },
+};
 </script>
 
 
 <style scoped>
-  @import "../../static/style.css";
+@import "../../static/style.css";
 </style>
