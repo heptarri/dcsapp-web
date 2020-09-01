25127c0178af3d21a795efe0c4f3fca68dc8d2dc
jerry <767729940@qq.com>
2020-09-01 11:00:15 +0800

    更改了首页链接

--- src/pages/Mainpage.vue
+++ src/pages/Mainpage.vue
@@ -15,6 +15,10 @@
           应用问题反馈: http://www.shenmo.tech:420/?p=419
           APP建议: http://www.shenmo.tech:420/?p=422
           国际排名：http://distrowatch.com/table.php?distribution=deepin
+          
+          商店官网: https://www.spark-app.store/
+          所有的问题反馈换成这统一的入口
+          反馈中心: https://www.deepinos.org/t/spark-feedback
       -->
         <!--
         <button @click="Search" class="bt-feedback" disabled>搜索应用(暂未开放)</button>
@@ -22,11 +26,9 @@
         -->
         <button @click="distrowatch" class="bt-feedback"><b>Deepin国际排名</b></button>
         <br>
-        <button @click="Dream" class="bt-feedback">愿望墙</button>
+        <button @click="officialwebsite" class="bt-feedback">商店官网</button>
         <br>
-        <button @click="FeedBack" class="bt-feedback">问题反馈</button>
-        <br>
-        <button @click="Proposal" class="bt-feedback">APP建议</button>
+        <button @click="FeedBack" class="bt-feedback">反馈中心</button>
         <br>
       </div>
     </center>
@@ -41,14 +43,11 @@
       return {}
     },
     methods: {
-      Dream: function () {
-        window.open("http://www.shenmo.tech:420/?p=91", "_self", "")
-      },
       FeedBack: function () {
-        window.open("http://www.shenmo.tech:420/?p=419", "_self", "")
+        window.open("https://www.deepinos.org/t/spark-feedback", "_self", "")
       },
-      Proposal: function () {
-        window.open("http://www.shenmo.tech:420/?p=422", "_self", "")
+      officialwebsite: function () {
+        window.open(" https://www.spark-app.store/", "_self", "")
       },
       distrowatch: function () {
         window.open("https://distrowatch.com/table.php?distribution=deepin", "_self", "")
